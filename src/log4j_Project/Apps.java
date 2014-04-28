24cc40c00606cbc1bcc90b280d4214ca0bcf5123
sarindy <sarindyo@yahoo.com>
2014-04-28 21:58:27 +0700

    Add Properties

--- src/log4j_Project/Apps.java
+++ src/log4j_Project/Apps.java
@@ -1,9 +1,15 @@
 package log4j_Project;
 
+import org.apache.log4j.Logger;
+
 public class Apps {
+	static Logger logger=Logger.getLogger(Apps.class.getName());
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+logger.debug("Debug Message");
+logger.info("Info Message");
 
 	}
 
