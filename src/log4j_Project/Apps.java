245d760987f5368187a1ae6002f5e4dca6d5430c
sarindy <sarindyo@yahoo.com>
2014-04-28 22:07:44 +0700

    1

--- src/log4j_Project/Apps.java
+++ src/log4j_Project/Apps.java
@@ -3,13 +3,12 @@ package log4j_Project;
 import org.apache.log4j.Logger;
 
 public class Apps {
-	static Logger logger=Logger.getLogger(Apps.class.getName());
-	
+	static Logger logger = Logger.getLogger(Apps.class.getName());
 
 	public static void main(String[] args) {
 
-logger.debug("Debug Message");
-logger.info("Info Message");
+		logger.debug("Debug Message");
+		logger.info("Info Message");
 
 	}
 
